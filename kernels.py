fd6f593cdbd23d64548b9414796aebc7f34b044b
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2018-02-15 15:58:07 +0100

    Working infrastructure for pressure solver

--- kernels.py
+++ kernels.py
@@ -1,20 +1,20 @@
 import numpy as np
 
 def no_slip(a, g):
-    a.data[g.kstart-1,:,:] = -1.*a.data[g.kstart,:,:]
-    a.data[g.kend    ,:,:] = -1.*a.data[g.kend-1,:,:]
+    a[g.kstart-1,:,:] = -1.*a[g.kstart,:,:]
+    a[g.kend    ,:,:] = -1.*a[g.kend-1,:,:]
 
 def no_penetration(a, g):
-    a.data[g.kstart,:,:] = 0.
-    a.data[g.kend  ,:,:] = 0.
-    a.data[g.kstart-1,:,:] = -1.*a.data[g.kstart+1,:,:]
-    a.data[g.kend+1  ,:,:] = -1.*a.data[g.kend-1,:,:]
+    a[g.kstart,:,:] = 0.
+    a[g.kend  ,:,:] = 0.
+    a[g.kstart-1,:,:] = -1.*a[g.kstart+1,:,:]
+    a[g.kend+1  ,:,:] = -1.*a[g.kend-1,:,:]
 
 def cyclic_boundaries(a, g):
-    a.data[:,:,0:g.istart] = a.data[:,:,g.iend-g.igc:g.iend]
-    a.data[:,0:g.jstart,:] = a.data[:,g.jend-g.jgc:g.jend,:]
-    a.data[:,:,g.iend:] = a.data[:,:,g.istart:g.istart+g.igc]
-    a.data[:,g.jend:,:] = a.data[:,g.jstart:g.jstart+g.jgc,:]
+    a[:,:,0:g.istart] = a[:,:,g.iend-g.igc:g.iend]
+    a[:,0:g.jstart,:] = a[:,g.jend-g.jgc:g.jend,:]
+    a[:,:,g.iend:] = a[:,:,g.istart:g.istart+g.igc]
+    a[:,g.jend:,:] = a[:,g.jstart:g.jstart+g.jgc,:]
 
 def i2(a, b):
     return 0.5*(a + b)
@@ -25,7 +25,7 @@ def advection_u(ut, u, v, w, g):
                     + ( i2(v[g.i( 0,+1,-1)], v[g.i( 0,+1, 0)]) * i2(u[g.i( 0, 0, 0)], u[g.i( 0,+1, 0)]) \
                       - i2(v[g.i( 0, 0,-1)], v[g.i( 0, 0, 0)]) * i2(u[g.i( 0,-1, 0)], u[g.i( 0, 0, 0)]) ) * g.dyi \
                     + ( i2(w[g.i(+1, 0,-1)], w[g.i(+1, 0, 0)]) * i2(u[g.i( 0, 0, 0)], u[g.i(+1, 0, 0)]) \
-                      - i2(w[g.i( 0, 0,-1)], w[g.i( 0, 0, 0)]) * i2(u[g.i(-1, 0, 0)], u[g.i( 0, 0, 0)]) ) * g.dzi[g.k(0),np.newaxis,np.newaxis]
+                      - i2(w[g.i( 0, 0,-1)], w[g.i( 0, 0, 0)]) * i2(u[g.i(-1, 0, 0)], u[g.i( 0, 0, 0)]) ) * g.dzi[g.k(0),None,None]
 
 def advection_v(vt, u, v, w, g):
     vt[g.i(0,0,0)] -= ( i2(u[g.i( 0,-1,+1)], u[g.i( 0, 0,+1)]) * i2(v[g.i( 0, 0, 0)], v[g.i( 0, 0,+1)]) \
@@ -33,7 +33,7 @@ def advection_v(vt, u, v, w, g):
                     + ( i2(v[g.i( 0, 0, 0)], v[g.i( 0,+1, 0)]) * i2(v[g.i( 0, 0, 0)], v[g.i( 0,+1, 0)]) \
                       - i2(v[g.i( 0,-1, 0)], v[g.i( 0, 0, 0)]) * i2(v[g.i( 0,-1, 0)], v[g.i( 0, 0, 0)]) ) * g.dyi \
                     + ( i2(w[g.i(+1, 0,-1)], w[g.i(+1, 0, 0)]) * i2(v[g.i( 0, 0, 0)], v[g.i(+1, 0, 0)]) \
-                      - i2(w[g.i( 0, 0,-1)], w[g.i( 0, 0, 0)]) * i2(v[g.i(-1, 0, 0)], v[g.i( 0, 0, 0)]) ) * g.dzi[g.k(0),np.newaxis,np.newaxis]
+                      - i2(w[g.i( 0, 0,-1)], w[g.i( 0, 0, 0)]) * i2(v[g.i(-1, 0, 0)], v[g.i( 0, 0, 0)]) ) * g.dzi[g.k(0),None,None]
 
 def advection_w(wt, u, v, w, g):
     wt[g.ih(0,0,0)] -= ( i2(u[g.ih( 0, 0, 0)], u[g.ih( 0, 0,+1)]) * i2(w[g.ih( 0, 0, 0)], w[g.ih( 0, 0,+1)]) \
@@ -41,7 +41,7 @@ def advection_w(wt, u, v, w, g):
                      + ( i2(v[g.ih( 0,+1,-1)], v[g.ih( 0,+1, 0)]) * i2(w[g.ih( 0, 0, 0)], w[g.ih( 0,+1, 0)]) \
                        - i2(v[g.ih( 0, 0,-1)], v[g.ih( 0, 0, 0)]) * i2(w[g.ih( 0,-1, 0)], w[g.ih( 0, 0, 0)]) ) * g.dyi \
                      + ( i2(w[g.ih( 0, 0, 0)], w[g.ih(+1, 0, 0)]) * i2(w[g.ih( 0, 0, 0)], w[g.ih(+1, 0, 0)]) \
-                       - i2(w[g.ih(-1, 0, 0)], w[g.ih( 0, 0, 0)]) * i2(w[g.ih(-1, 0, 0)], w[g.ih( 0, 0, 0)]) ) * g.dzhi[g.kh(0),np.newaxis,np.newaxis]
+                       - i2(w[g.ih(-1, 0, 0)], w[g.ih( 0, 0, 0)]) * i2(w[g.ih(-1, 0, 0)], w[g.ih( 0, 0, 0)]) ) * g.dzhi[g.kh(0),None,None]
 
 def diffusion(at, a, visc, g):
     dxidxi = g.dxi**2
@@ -51,8 +51,8 @@ def diffusion(at, a, visc, g):
                       - (a[g.i( 0, 0, 0)] - a[g.i( 0, 0,-1)]) ) * dxidxi \
                     + ( (a[g.i( 0,+1, 0)] - a[g.i( 0, 0, 0)]) \
                       - (a[g.i( 0, 0, 0)] - a[g.i( 0,-1, 0)]) ) * dyidyi \
-                    + ( (a[g.i(+1, 0, 0)] - a[g.i( 0, 0, 0)]) * g.dzhi[g.k(+1),np.newaxis,np.newaxis] \
-                      - (a[g.i( 0, 0, 0)] - a[g.i(-1, 0, 0)]) * g.dzhi[g.k( 0),np.newaxis,np.newaxis] ) * g.dzi[g.k(0),np.newaxis,np.newaxis] )
+                    + ( (a[g.i(+1, 0, 0)] - a[g.i( 0, 0, 0)]) * g.dzhi[g.k(+1),None,None] \
+                      - (a[g.i( 0, 0, 0)] - a[g.i(-1, 0, 0)]) * g.dzhi[g.k( 0),None,None] ) * g.dzi[g.k(0),None,None] )
 
 def diffusion_w(at, a, visc, g):
     dxidxi = g.dxi**2
@@ -62,34 +62,28 @@ def diffusion_w(at, a, visc, g):
                        - (a[g.ih( 0, 0, 0)] - a[g.ih( 0, 0,-1)]) ) * dxidxi \
                      + ( (a[g.ih( 0,+1, 0)] - a[g.ih( 0, 0, 0)]) \
                        - (a[g.ih( 0, 0, 0)] - a[g.ih( 0,-1, 0)]) ) * dyidyi \
-                     + ( (a[g.ih(+1, 0, 0)] - a[g.ih( 0, 0, 0)]) * g.dzi[g.kh( 0),np.newaxis,np.newaxis] \
-                       - (a[g.ih( 0, 0, 0)] - a[g.ih(-1, 0, 0)]) * g.dzi[g.kh(-1),np.newaxis,np.newaxis] ) * g.dzhi[g.kh(0),np.newaxis,np.newaxis] )
+                     + ( (a[g.ih(+1, 0, 0)] - a[g.ih( 0, 0, 0)]) * g.dzi[g.kh( 0),None,None] \
+                       - (a[g.ih( 0, 0, 0)] - a[g.ih(-1, 0, 0)]) * g.dzi[g.kh(-1),None,None] ) * g.dzhi[g.kh(0),None,None] )
 
-#    // set the cyclic boundary conditions for the tendencies
-#    grid->boundary_cyclic(ut, East_west_edge  );
-#    grid->boundary_cyclic(vt, North_south_edge);
-#
-#    // write pressure as a 3d array without ghost cells
-#    for (int k=0; k<grid->kmax; k++)
-#        for (int j=0; j<grid->jmax; j++)
-##pragma ivdep
-#            for (int i=0; i<grid->imax; i++)
-#            {
-#                const int ijkp = i + j*jjp + k*kkp;
-#                const int ijk  = i+igc + (j+jgc)*jj + (k+kgc)*kk;
-#                p[ijkp] = rhoref[k+kgc] * ( (ut[ijk+ii] + u[ijk+ii] * dti) - (ut[ijk] + u[ijk] * dti) ) * dxi
-#                        + rhoref[k+kgc] * ( (vt[ijk+jj] + v[ijk+jj] * dti) - (vt[ijk] + v[ijk] * dti) ) * dyi
-#                        + ( rhorefh[k+kgc+1] * (wt[ijk+kk] + w[ijk+kk] * dti) 
-#                          - rhorefh[k+kgc  ] * (wt[ijk   ] + w[ijk   ] * dti) ) * dzi[k+kgc];
-#            }
+def pressure_solve(p, u, v, w, ut, vt, wt, g, dt):
+    cyclic_boundaries(ut, g)
+    cyclic_boundaries(vt, g)
 
-def pressure_input(p, u, v, w, u_tend, v_tend, w_tend, g):
-    cyclic_boundaries(u_tend, g)
-    cyclic_boundaries(v_tend, g)
+    tmp = ( (ut[g.i( 0, 0,+1)] + u[g.i( 0, 0,+1)]/dt) - (ut[g.i( 0, 0, 0)] + u[g.i( 0, 0, 0)]/dt) ) * g.dxi \
+        + ( (vt[g.i( 0,+1, 0)] + v[g.i( 0,+1, 0)]/dt) - (vt[g.i( 0, 0, 0)] + v[g.i( 0, 0, 0)]/dt) ) * g.dyi \
+        + ( (wt[g.i(+1, 0, 0)] + w[g.i(+1, 0, 0)]/dt) - (wt[g.i( 0, 0, 0)] + w[g.i( 0, 0, 0)]/dt) ) * g.dzi[g.k(0), None, None]
 
-def pressure_solve():
-    return
+    tmp = np.fft.rfft2(tmp)
 
-def pressure_tendency():
-    return
+    tmp = np.fft.irfft2(tmp)
+
+    p[g.i( 0, 0, 0)] = tmp[:,:,:]
+
+    # Set a zero gradient at the wall.
+    p[g.kstart-1,:,:] = p[g.kstart,:,:]
+
+def pressure_tendency(ut, vt, wt, p, g):
+    ut[g.i ( 0, 0, 0)] -= (p[g.i ( 0, 0,+1)] - p[g.i ( 0, 0, 0)]) * g.dxi
+    vt[g.i ( 0, 0, 0)] -= (p[g.i ( 0,+1, 0)] - p[g.i ( 0, 0, 0)]) * g.dyi
+    wt[g.ih( 0, 0, 0)] -= (p[g.ih(+1, 0, 0)] - p[g.ih( 0, 0, 0)]) * g.dzhi[g.kh(0), None, None]
 
--- microhh.py
+++ microhh.py
@@ -43,14 +43,14 @@ w.randomize(1e-3)
 
 for n in range(1):
     # Calculate right-hand side.
-    kernels.no_slip(u, g)
-    kernels.no_slip(v, g)
+    kernels.no_slip(u.data, g)
+    kernels.no_slip(v.data, g)
     
-    kernels.no_penetration(w, g)
+    kernels.no_penetration(w.data, g)
     
-    kernels.cyclic_boundaries(u, g)
-    kernels.cyclic_boundaries(v, g)
-    kernels.cyclic_boundaries(w, g)
+    kernels.cyclic_boundaries(u.data, g)
+    kernels.cyclic_boundaries(v.data, g)
+    kernels.cyclic_boundaries(w.data, g)
     
     kernels.advection_u(u_tend.data, u.data, v.data, w.data, g)
     kernels.advection_v(v_tend.data, u.data, v.data, w.data, g)
@@ -59,10 +59,10 @@ for n in range(1):
     kernels.diffusion  (u_tend.data, u.data, visc, g)
     kernels.diffusion  (v_tend.data, v.data, visc, g)
     kernels.diffusion_w(w_tend.data, w.data, visc, g)
-    
-    kernels.pressure_input(p, u, v, w, u_tend, v_tend, w_tend, g)
-    kernels.pressure_solve()
-    kernels.pressure_tendency()
+   
+    dt = 0.001
+    kernels.pressure_solve(p.data, u.data, v.data, w.data, u_tend.data, v_tend.data, w_tend.data, g, dt)
+    kernels.pressure_tendency(u_tend.data, v_tend.data, w_tend.data, p.data, g)
     # End of right-hand side.
 
 print('Finished')
